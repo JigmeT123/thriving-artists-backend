cd5aaa449200b014993c795d1f62487224bd2c05
jigmet12 <jigmetashi02@gmail.com>
2020-12-20 14:29:06 +0400

    FEAT: connect with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ArtistsModule } from './artists/artists.module';
+import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ArtistsModule],
+  imports: [ArtistsModule, TypeOrmModule.forRoot(typeOrmConfig)],
 })
 export class AppModule {}
