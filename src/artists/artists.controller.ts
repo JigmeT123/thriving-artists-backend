27a0f4151d3528e797b3d25f3322d8ba018d5fa9
jigmet12 <jigmetashi02@gmail.com>
2020-12-20 14:25:01 +0400

    FEAT: entity, controller, service, modules

---
+++ src/artists/artists.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common';
+import {ArtistsService} from './artists.service';
+
+@Controller('artists')
+export class ArtistsController {
+    constructor(
+        private readonly artistService: ArtistsService,
+    ){}
+
+    
+}
---
+++ src/artists/artists.entity.ts
@@ -0,0 +1,59 @@
+import {BaseEntity, PrimaryGeneratedColumn, Column} from 'typeorm';
+
+export class ArtistsEntity extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    artistName: string;
+
+    @Column()
+    artistInsta:string;
+
+    @Column()
+    artistWp:string;
+
+    @Column()
+    artistEmail:string;
+
+    @Column()
+    artistBio:string;
+
+    @Column()
+    art1Name:string;
+    @Column()
+    artDec1:string;
+    @Column()
+    art1Price:number;
+
+    @Column()
+    art2Name:string;
+    @Column()
+    artDec2:string;
+    @Column()
+    art2Price:number;
+
+    @Column()
+    art3Name:string;
+    @Column()
+    artDec3:string;
+    @Column()
+    art3Price:number;
+
+    @Column()
+    art4Name:string;
+    @Column()
+    artDec4:string;
+    @Column()
+    art4Price:number;
+
+    @Column()
+    art5Name:string;
+    @Column()
+    artDec5:string;
+    @Column()
+    art5Price:number;
+
+    @Column()
+    sold:boolean;
+}
